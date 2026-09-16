5ee254c121e0525d8c664beda74303f38107e9db
devopslabks <kdevopss1@gmail.com>
2026-09-16 05:00:28 +0000

    Add k8s deployemnt and service

--- server_status.py
+++ server_status.py
@@ -1,14 +1,25 @@
 
-import time
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 servers = ["web1", "web2", "web3"]
 
-while True:
-    for server in servers:
-        print(server + " is Running")
+class ServerStatus(BaseHTTPRequestHandler):
 
-    time.sleep(10)
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
 
+        message = ""
+        for server in servers:
+            message += server + " is Running\n"
 
+        self.wfile.write(message.encode())
+
+server = HTTPServer(("0.0.0.0", 8080), ServerStatus)
+
+print("Server Status application running on port 8080")
+
+server.serve_forever()
 
 
